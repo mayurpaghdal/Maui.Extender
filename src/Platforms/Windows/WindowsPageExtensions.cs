0efb339c98a4918704e9f5de98311ea8425cfb60
Mayur P <mayur.paghdal@baliosoft.biz>
2024-02-05 16:42:09 +0530

    added windows backgrounding ref. (future ref.)

---
+++ src/Platforms/Windows/WindowsPageExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Windows.ApplicationModel.Background;
+
+namespace Maui.Extender.Backgrounding;
+
+
+public class BackgroundAggregator
+{
+    private static string BackServiceName = "BackgroundService";
+
+    public static void Init(MauiWinUIApplication page)
+    {
+        WeakReferenceMessenger.Default.Register<StartLongRunningTaskMessage>(page, async (r, message) =>
+            {
+                var access = await BackgroundExecutionManager.RequestAccessAsync();
+
+                switch (access)
+                {
+                    case BackgroundAccessStatus.Unspecified:
+                        return;
+                    case BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity:
+                        return;
+                    case BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity:
+                        return;
+                    case BackgroundAccessStatus.Denied:
+                        return;
+                }
+
+                var task = new BackgroundTaskBuilder
+                {
+                    Name = BackServiceName,
+                };
+
+                var trigger = new ApplicationTrigger();
+                task.SetTrigger(trigger);
+
+                //var condition = new SystemCondition(SystemConditionType.InternetAvailable);
+                task.Register();
+
+                await trigger.RequestAsync();
+            });
+
+        WeakReferenceMessenger.Default.Register<StopLongRunningTaskMessage>(page, (r, message) =>
+            {
+                var tasks = BackgroundTaskRegistration.AllTasks;
+                foreach (var task in tasks)
+                {
+                    // You can check here for the name
+                    string name = task.Value.Name;
+                    if (name == BackServiceName)
+                    {
+                        task.Value.Unregister(true);
+                    }
+                }
+            });
+    }
+}
